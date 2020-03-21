51491fae979c53d059ea50ecabdf6df585e479cb
abiryusuf <yusuf_ctg@yahoo.com>
2020-03-20 23:39:56 -0400

    updateAPI

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 
 /*  Weather API */
 var OneWordClimate
-
+var currLocation;
+var apiKey = "166a433c57516f51dfab1f7edaed8413"
 function CallWeather() {
+    var 
     var APICity = "Sydney";
     console.log(APICity)
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+APICity+"&units=imperial&appid=166a433c57516f51dfab1f7edaed8413";
+    //add new api
+    var queryURL1 = "https:/api.openweathermap.org/data/2.5/weather?q=" + currLocation + " &appid=" + apiKey;
     $.ajax({
-        url: queryURL,
+        url: queryURL1,
         method: "GET"
     }).then(function(response) {            
      OneWordClimate = response.weather[0].main
